33413265ede7b2a107d0cea03ac19345884adc23
Nirmal <lynchamika@gmail.com>
2020-12-31 18:02:32 +0530

    +keells dashbaord 7

--- Keells Dashboard/flutter_service_worker.js
+++ Keells Dashboard/flutter_service_worker.js
@@ -6,14 +6,14 @@ const RESOURCES = {
   "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"assets/NOTICES": "286df8bbafa25f43d9f1ea1f994a8f1f",
+"assets/NOTICES": "f926abcb1fdb563c20deb73ed81f1246",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "f75887c3d4e54d9b9049b722bca9be92",
-"/": "f75887c3d4e54d9b9049b722bca9be92",
-"main.dart.js": "e0963c3bb2088067ac94203323edeb41",
+"index.html": "9e8917bf9d06b9388c195b6f13d5bd8b",
+"/": "9e8917bf9d06b9388c195b6f13d5bd8b",
+"main.dart.js": "f835e51c442efe568e96bcbb0b086d76",
 "manifest.json": "72e3094dea0e06d3fc94db683f4da79e",
 "version.json": "9bed3d239e84428da794b9c03ef2b7b8"
 };
