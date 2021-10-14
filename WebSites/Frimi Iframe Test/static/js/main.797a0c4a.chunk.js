9ca20a0d472e534b4c68cc4bab9ac561609cc8db
Nirmal <lynchamika@gmail.com>
2021-10-15 04:14:27 +0530

    +frimi iframe test 2

--- WebSites/Frimi Iframe Test/static/js/main.797a0c4a.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpfrimi_iframe_test=this.webpackJsonpfrimi_iframe_test||[]).push([[0],{9:function(e,t,i){"use strict";i.r(t);var n=i(1),s=i.n(n),c=i(3),o=i.n(c),r=i(0);var d=function(){return Object(r.jsxs)("div",{children:[Object(r.jsx)("div",{style:{height:100}}),Object(r.jsx)("button",{onClick:function(){console.log(window),window.postMessage("Hello","*")},children:"Click"})]})};o.a.render(Object(r.jsx)(s.a.StrictMode,{children:Object(r.jsx)(d,{})}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.797a0c4a.chunk.js.map
