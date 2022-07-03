9689bcca071dbfc0ac4ed366cd4c1d1e29612cd5
Nirmal <lynchamika@gmail.com>
2022-07-03 11:53:51 +0530

    +tic tac toe  minimax added

--- WebSites/Frimi Iframe Test/static/js/main.e7909410.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpfrimi_iframe_test=this.webpackJsonpfrimi_iframe_test||[]).push([[0],{9:function(e,t,i){"use strict";i.r(t);var n=i(1),s=i.n(n),c=i(3),o=i.n(c),r=i(0);var a=function(){return Object(r.jsxs)("div",{children:[Object(r.jsx)("div",{style:{height:100}}),Object(r.jsx)("button",{onClick:function(){console.log("callHome asd"),window.top.postMessage("HOME","*")},children:"Click Me"})]})};o.a.render(Object(r.jsx)(s.a.StrictMode,{children:Object(r.jsx)(a,{})}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.e7909410.chunk.js.map
